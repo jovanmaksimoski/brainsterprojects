4e9875b6927c1dd4e08e9a53e06242a2df275960
Jovan Maksimoski <jovanmaksimoskii@gmail.com>
2024-06-11 20:58:59 +0200

    sadas

---
+++ backEnd/Classes/loginUser.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace backEnd\Classes;
+
+require_once ("dbConnection.php");
+
+class LoginUser
+{
+    protected \PDO $_db;
+
+    public function __construct(\PDO $db)
+    {
+        $this->_db = $db;
+    }
+
+    public function login($email, $password)
+    {
+        $stmt = $this->_db->prepare("SELECT * FROM users WHERE email = ?");
+        $stmt->execute([$email]);
+        $user = $stmt->fetch(\PDO::FETCH_ASSOC);
+
+        if (!$user) {
+            return "User not found";
+        }
+
+        if (!password_verify($password, $user['password'])) {
+            return "Incorrect password";
+        }
+        return true;
+    }
+};
+
+session_start();
+
+if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["login"])) {
+    $dbConnection = new DbConnection(); // Instantiate DbConnection
+    $db = $dbConnection->getDbConnection();
+    $loginUser = new LoginUser($db);
+    $email = $_POST["email"];
+    $password = $_POST["password"];
+    $result = $loginUser->login($email, $password);
+
+    if ($result === true) {
+        $_SESSION['success_message'] = "Logged in successfully";
+        header("Location: ../../index.php");
+        exit;
+    } else {
+        $_SESSION['error_message'] = $result;
+        header("Location: ../../login.php");
+        exit;
+    }
+}
+
--- login.php
+++ login.php
@@ -5,7 +5,7 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Login </title>
     <script src="https://cdn.tailwindcss.com"></script>
 
 </head>
@@ -13,14 +13,22 @@
 
 <!--Log in page -->
 
-<section class="bg-gray-50 dark:bg-gray-900">
+<form class="bg-gray-50 dark:bg-gray-900" action="backEnd/Classes/loginUser.php" method="POST">
     <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
         <a href="#" class="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
             <img class=" h-14 " src="../img/dw.webp" alt="Brainster Logo">
 
-
         </a>
         <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+            <?php
+            session_start();
+
+            if (isset($_SESSION['error_message'])) {
+                echo '<div class="text-red-400">' . $_SESSION['error_message'] . '</div>';
+                unset($_SESSION['error_message']);
+            }
+
+            ?>
             <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
                 <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
                     Log In
@@ -41,7 +49,7 @@
                     </div>
 
 
-                    <button type="submit"
+                    <button type="submit" name="create"
                             class="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">
                         Log In
                     </button>
@@ -56,7 +64,7 @@
             </div>
         </div>
     </div>
-</section>
+</form>
 </body>
 </html>
 
