eb35e1ae8a092e9da87d42a1ece985743d6e3ccf
Jovan Maksimoski <jovanmaksimoskii@gmail.com>
2024-06-11 16:31:44 +0200

    working on signup

---
+++ backEnd/Classes/dbConnection.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace backEnd\Classes;
+
+class DbConnection {
+    const DB_HOSTNAME = 'localhost';
+    const DB_USERNAME = 'root';
+    const DB_PASSWORD = '';
+    const DB_NAME = 'brainster_library';
+    protected \PDO $_db_connect;
+
+
+    public function __construct() {
+        $dsn = "mysql:host=" . self::DB_HOSTNAME . ";dbname=" . self::DB_NAME;
+
+
+        try {
+            $this->_db_connect = new \PDO($dsn, self::DB_USERNAME, self::DB_PASSWORD);
+            $this->_db_connect->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+        } catch(\PDOException $e) {
+            die("Connection failed: " . $e->getMessage());
+        }
+    }
+
+    public function getDbConnection() {
+        return $this->_db_connect;
+    }
+}
+
